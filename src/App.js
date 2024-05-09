f557cf42fb261a035ba02243c108c4552861478a
jumihongsee <juju_32@naver.com>
2024-05-09 17:47:54 +0900

    복습용 첫번째

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar , Container, Nav, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+import data from './data.js'
+//import backgroundImg from './img/shop-bg.png';
+
 import './App.css';
 
 function App() {
+
+  //Public 폴더 안에 있는 파일들은 build 할때 압축되지 않아서 변형걱정없음 그래서 이미지 파일을 여따 넣는 것 임.
+  //경로는 슬래쉬 부터 시작하고 넣으면 댐 
+  //Public 폴더 이미지 사용하는 권장 방식  <img src={process.env.PUBLIC_URL + '/logo192.png'} /> 
+
+
+  let [shoes] = useState(data);
+
+  console.log(shoes)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ 
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ZoomShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#home">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {/* <div className='main-bg' style={{backgroundImage : `url(${backgroundImg})`}}></div> */}
+      <div className='main-bg' style={{backgroundImage : `url(${process.env.PUBLIC_URL}/shop-bg.png)`}}></div>
+
+      <Container>
+      <Row>
+        <Col>
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col>
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+        <Col>
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+        </Col>
+      </Row>
+    </Container>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,29 @@
+let data = [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 120000
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 110000
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 130000
+    }
+  ] 
+
+
+  // 보내고 싶은 변수가 하나면 export default
+  // 여러개면 export만
+  // 자료 말고 컴포넌트 함수도 export도 가능하다.
+  
+export default data;
